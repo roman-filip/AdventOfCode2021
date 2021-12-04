7beab41eaa779243cac4e302084cd81af1f86fe0
Roman FILIP <dicky@email.cz>
2021-12-05 00:31:18 +0100

    Implement task nr. 2 of day 1

--- RFI.AdventOfCode2021/Day1/SonarSweep.cs
+++ RFI.AdventOfCode2021/Day1/SonarSweep.cs
@@ -1,6 +1,5 @@
 ﻿namespace RFI.AdventOfCode2021.Day1
 {
-
     /// <summary>
     /// As the submarine drops below the surface of the ocean, it automatically performs a sonar sweep of the nearby sea floor.
     /// On a small screen, the sonar sweep report (your puzzle input) appears: each line is a measurement of the sea floor 
@@ -36,6 +35,37 @@
     /// In this example, there are 7 measurements that are larger than the previous measurement.
     ///
     /// How many measurements are larger than the previous measurement?
+    /// 
+    /// 
+    /// --- Part Two ---
+    /// Considering every single measurement isn't as useful as you expected: there's just too much noise in the data.
+    /// Instead, consider sums of a three-measurement sliding window.Again considering the above example:
+    /// 199  A
+    /// 200  A B    
+    /// 208  A B C
+    /// 210    B C D
+    /// 200  E   C D
+    /// 207  E F   D
+    /// 240  E F G
+    /// 269    F G H
+    /// 260      G H
+    /// 263        H
+    /// Start by comparing the first and second three-measurement windows. The measurements in the first window are marked A (199, 200, 208); their sum is 199 + 200 + 208 = 607.
+    /// The second window is marked B(200, 208, 210); its sum is 618. The sum of measurements in the second window is larger than the sum of the first, so this first comparison increased.
+    /// Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum.So, compare A with B, 
+    /// then compare B with C, then C with D, and so on.Stop when there aren't enough measurements left to create a new three-measurement sum.
+    ///
+    /// In the above example, the sum of each three-measurement window is as follows:
+    /// A: 607 (N/A - no previous sum)
+    /// B: 618 (increased)
+    /// C: 618 (no change)
+    /// D: 617 (decreased)
+    /// E: 647 (increased)
+    /// F: 716 (increased)
+    /// G: 769 (increased)
+    /// H: 792 (increased)
+    /// In this example, there are 5 sums that are larger than the previous sum.
+    /// Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
     /// </summary>
     public class SonarSweep
     {
@@ -43,7 +73,7 @@
         {
             var count = 0;
 
-            if (measurements != null && measurements.Count() > 1)
+            if (measurements?.Count() > 1)
             {
                 var m = measurements.ToArray();
                 for (int i = 1; i < m.Length; i++)
@@ -52,6 +82,29 @@
                     {
                         count++;
                     }
+                }
+            }
+
+            return count;
+        }
+
+        public int GetCountOfIncreasedMeasurementsInSlidingWindow(IEnumerable<int> measurements)
+        {
+            var count = 0;
+
+            if (measurements?.Count() > 1)
+            {
+                var prevSum = int.MaxValue;
+                var m = measurements.ToArray();
+
+                for (int i = 0; i < m.Length - 2; i++)
+                {
+                    var sum = m[i] + m[i + 1] + m[i + 2];
+                    if (sum > prevSum)
+                    {
+                        count++;
+                    }
+                    prevSum = sum;
                 }
             }
 
