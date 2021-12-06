499232988a6a307329d38815f7649bb92e725b39
Roman FILIP <dicky@email.cz>
2021-12-06 23:31:37 +0100

    Implement task nr. 2 of day 2

--- RFI.AdventOfCode2021/Day2/Submarine.cs
+++ RFI.AdventOfCode2021/Day2/Submarine.cs
@@ -28,8 +28,41 @@
     /// After following these instructions, you would have a horizontal position of 15 and a depth of 10. (Multiplying these together produces 150.)
     /// 
     /// Calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
+    /// 
+    /// --- Part Two ---
+    /// Based on your calculations, the planned course doesn't seem to make any sense. You find the submarine manual and discover that the process is actually slightly more complicated.
+    /// 
+    /// In addition to horizontal position and depth, you'll also need to track a third value, aim, which also starts at 0. The commands also mean something entirely different
+    /// than you first thought: 
+    ///     down X increases your aim by X units.
+    ///     up X decreases your aim by X units.
+    ///     forward X does two things:
+    ///         It increases your horizontal position by X units.
+    ///         It increases your depth by your aim multiplied by X.
+    /// 
+    /// Again note that since you're on a submarine, down and up do the opposite of what you might expect: "down" means aiming in the positive direction.
+    /// 
+    /// Now, the above example does something different:
+    ///     forward 5 adds 5 to your horizontal position, a total of 5. Because your aim is 0, your depth does not change.
+    ///     down 5 adds 5 to your aim, resulting in a value of 5.
+    ///     forward 8 adds 8 to your horizontal position, a total of 13. Because your aim is 5, your depth increases by 8*5=40.
+    ///     up 3 decreases your aim by 3, resulting in a value of 2.
+    ///     down 8 adds 8 to your aim, resulting in a value of 10.
+    ///     forward 2 adds 2 to your horizontal position, a total of 15. Because your aim is 10, your depth increases by 2*10=20 to a total of 60.
+    /// 
+    /// After following these new instructions, you would have a horizontal position of 15 and a depth of 60. (Multiplying these produces 900.)
+    /// 
+    /// Using this new interpretation of the commands, calculate the horizontal position and depth you would have after following the planned course.
+    /// What do you get if you multiply your final horizontal position by your final depth?
     public class Submarine
     {
+        private enum CommandType
+        {
+            Down,
+            Up,
+            Forward
+        }
+
         public int Dive(IEnumerable<string> commands)
         {
             var depth = 0;
@@ -39,25 +72,64 @@
             {
                 foreach (var command in commands)
                 {
-                    var cmd = command.Split(" ");
-                    switch (cmd[0])
+                    (CommandType cmdType, int cmdValue) = ParseCommand(command);
+                    switch (cmdType)
                     {
-                        case "forward":
-                            horizontalPosition += int.Parse(cmd[1]);
+                        case CommandType.Forward:
+                            horizontalPosition += cmdValue;
                             break;
-                        case "down":
-                            depth += int.Parse(cmd[1]);
+                        case CommandType.Down:
+                            depth += cmdValue;
                             break;
-                        case "up":
-                            depth -= int.Parse(cmd[1]);
+                        case CommandType.Up:
+                            depth -= cmdValue;
                             break;
-                        default:
-                            throw new Exception("Unsupported command.");
                     }
                 }
             }
 
             return depth * horizontalPosition;
+        }
+
+        public int DiveWithAim(IEnumerable<string> commands)
+        {
+            var depth = 0;
+            var horizontalPosition = 0;
+            var aim = 0;
+
+            if (commands != null)
+            {
+                foreach (var command in commands)
+                {
+                    (CommandType cmdType, int cmdValue) = ParseCommand(command);
+                    switch (cmdType)
+                    {
+                        case CommandType.Forward:
+                            horizontalPosition += cmdValue;
+                            depth += aim * cmdValue;
+                            break;
+                        case CommandType.Down:
+                            aim += cmdValue;
+                            break;
+                        case CommandType.Up:
+                            aim -= cmdValue;
+                            break;
+                    }
+                }
+            }
+
+            return depth * horizontalPosition;
+        }
+
+        private static (CommandType type, int value) ParseCommand(string command)
+        {
+            var parsedCmd = command.Split(" ");
+            if (parsedCmd.Length != 2)
+            {
+                throw new ArgumentException($"Wrong format of command: '{command}'", nameof(command));
+            }
+
+            return (Enum.Parse<CommandType>(parsedCmd[0], true), int.Parse(parsedCmd[1]));
         }
     }
 }
