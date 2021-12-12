a0ae34a815257a7bc808a34e1ebb530fc1d9aa1e
Roman FILIP <dicky@email.cz>
2021-12-12 19:08:39 +0100

    Implement task nr. 1 of day 4

---
+++ RFI.AdventOfCode2021.Tests/Day4/GiantSquidBingoTests.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RFI.AdventOfCode2021.Day4.Tests
+{
+    [TestClass()]
+    public class GiantSquidBingoTests
+    {
+        private readonly GiantSquidBingo _giantSquidBingo = new();
+
+        [TestMethod()]
+        public void PlayTest_TrainingInput()
+        {
+            var result = _giantSquidBingo.Play(File.ReadAllLines(@"Day4\TrainingInput.txt"));
+            Assert.AreEqual(4512, result);
+        }
+
+        [TestMethod()]
+        public void PlayTest_RealInput()
+        {
+            var result = _giantSquidBingo.Play(File.ReadAllLines(@"Day4\RealInput.txt"));
+            Assert.AreEqual(666, result);
+        }
+    }
+}
---
+++ RFI.AdventOfCode2021/Day4/BingoBoard.cs
@@ -0,0 +1,26 @@
+﻿namespace RFI.AdventOfCode2021.Day4
+{
+    internal class BingoBoard
+    {
+        private int[][] _boardConfiguration;
+
+        public BingoBoard(int[][] boardConfiguration)
+        {
+            _boardConfiguration = boardConfiguration;
+        }
+
+        public void MarkNumber(int number)
+        {
+        }
+
+        public bool IsBingo()
+        {
+            return false;
+        }
+
+        public int GetScore()
+        {
+            return 0;
+        }
+    }
+}
