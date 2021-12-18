48c830bbb4b8156c68b0880e98bab20823a2ec26
Roman FILIP <dicky@email.cz>
2021-12-18 23:14:06 +0100

    Implement task nr. 1 of day 5

---
+++ RFI.AdventOfCode2021.Tests/Day5/HydrothermalVentureTests.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RFI.AdventOfCode2021.Day5.Tests
+{
+    [TestClass()]
+    public class HydrothermalVentureTests
+    {
+        private readonly HydrothermalVenture _hydrothermalVenture = new();
+
+        [TestMethod()]
+        public void GetOverlapsCountTest_TrainingInput()
+            => Assert.AreEqual(5, _hydrothermalVenture.GetOverlapsCount(File.ReadAllLines(@"Day5\TrainingInput.txt")));
+
+        [TestMethod()]
+        public void GetOverlapsCountTest_RealInput()
+            => Assert.AreEqual(5, _hydrothermalVenture.GetOverlapsCount(File.ReadAllLines(@"Day5\RealInput.txt")));
+    }
+}
---
+++ RFI.AdventOfCode2021/Day5/VentLine.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RFI.AdventOfCode2021.Day5
+{
+    internal record Point(int Col, int Row);
+
+    internal class VentLine
+    {
+        private static Regex _rx = new(@"^(?<c1>\d+),(?<r1>\d+) -> (?<c2>\d+),(?<r2>\d+)$", RegexOptions.Compiled);
+
+        public Point Start { get; set; }
+
+        public Point End { get; set; }
+
+        public VentLine(string ventLine)
+        {
+            var matches = _rx.Matches(ventLine);
+            if (matches.Count != 1)
+            {
+                throw new Exception("Could not parse vent line");
+            }
+
+            Start = new Point(int.Parse(matches[0].Groups["c1"].Value), int.Parse(matches[0].Groups["r1"].Value));
+            End = new Point(int.Parse(matches[0].Groups["c2"].Value), int.Parse(matches[0].Groups["r2"].Value));
+        }
+    }
+}
