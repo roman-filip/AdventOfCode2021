371ca4127c84eda84f9136c21634fabbb2c4d118
Roman FILIP <dicky@email.cz>
2022-01-29 15:31:43 +0100

    Implement task nr. 1 of day 8

---
+++ RFI.AdventOfCode2021.Tests/Day8/SevenSegmentSearchTests.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RFI.AdventOfCode2021.Day8;
+
+namespace RFI.AdventOfCode2021.Tests.Day8
+{
+    [TestClass]
+    public class SevenSegmentSearchTests
+    {
+        private const string TRAINING_INPUT = @"Day8\TrainingInput.txt";
+        private const string REAL_INPUT = @"Day8\RealInput.txt";
+
+        private readonly SevenSegmentSearch _sevenSegmentSearch = new();
+
+        [TestMethod]
+        [DataRow(26, TRAINING_INPUT)]
+        [DataRow(409, REAL_INPUT)]
+        public void GetCountOf_1_4_7Test(int expectedCount, string file)
+            => Assert.AreEqual(expectedCount, _sevenSegmentSearch.GetCountOf_1_4_7_8(File.ReadAllLines(file)));
+    }
+}
