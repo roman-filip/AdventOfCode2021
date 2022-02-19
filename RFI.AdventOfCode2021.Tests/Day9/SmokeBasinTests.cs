edb48b9d1b27180fb2665fb95a5ec11385ead633
Roman FILIP <dicky@email.cz>
2022-02-19 14:44:02 +0100

    Implement task nr. 1 of day 9

---
+++ RFI.AdventOfCode2021.Tests/Day9/SmokeBasinTests.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RFI.AdventOfCode2021.Day9;
+
+namespace RFI.AdventOfCode2021.Tests.Day9
+{
+    [TestClass]
+    public class SmokeBasinTests
+    {
+        private const string TRAINING_INPUT = @"Day9\TrainingInput.txt";
+        private const string REAL_INPUT = @"Day9\RealInput.txt";
+
+        private readonly SmokeBasin _smokeBasin = new();
+
+        [TestMethod]
+        [DataRow(15, TRAINING_INPUT)]
+        [DataRow(518, REAL_INPUT)]
+        public void ComputeRiskLevelTest(int expectedCount, string file)
+            => Assert.AreEqual(expectedCount, _smokeBasin.ComputeRiskLevel(File.ReadAllLines(file)));
+    }
+}
